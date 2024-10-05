e9dafee917fe7a7ba2ad7bff7d085ff20557fdcb
alikurules <alikurules@gmail.com>
2024-10-05 06:38:00 +0300

    Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -2,19 +2,13 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = False
+        self._is_checked_out = False 
 
     def check_out(self):
-        if not self._is_checked_out:
-            self._is_checked_out = True
-            return True
-        return False
+        self._is_checked_out = True
 
     def return_book(self):
-        if self._is_checked_out:
-            self._is_checked_out = False
-            return True
-        return False
+        self._is_checked_out = False
 
     def is_available(self):
         return not self._is_checked_out
@@ -30,22 +24,25 @@ class Library:
     def check_out_book(self, title):
         for book in self._books:
             if book.title == title and book.is_available():
-                return book.check_out()
-        return False
+                book.check_out()
+                return
+        print(f"Book '{title}' is not available or not found.")
 
     def return_book(self, title):
         for book in self._books:
             if book.title == title and not book.is_available():
-                return book.return_book()
-        return False
+                book.return_book()
+                return
+        print(f"Book '{title}' is already available or not found.")
 
     def list_available_books(self):
-        available = [f"{book.title} by {book.author}" for book in self._books if book.is_available()]
-        return available
-
-        
-
-        
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(f"{book.title} by {book.author}")
+        else:
+            print("No books are currently available.")
+       
 
         
 
