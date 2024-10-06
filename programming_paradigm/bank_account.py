a395090eff5a86243aaed177ad5a259e43a9e5d1
alikurules <alikurules@gmail.com>
2024-10-06 21:02:06 +0300

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,29 +1,6 @@
-# class BankAccount:
-#     def __init__(self, initial_balance=0.0):
-#         self.__account_balance = initial_balance
-
-#     def deposit(self, amount):
-#         if amount <= 0:
-#             print("Deposit amount must be positive.")
-#             return
-#         self.__account_balance += amount
-
-#     def withdraw(self, amount):
-#         if amount <= 0:
-#             print("Withdrawal amount must be positive.")
-#             return False
-#         if amount > self.__account_balance:
-#             return False
-#         self.__account_balance -= amount
-#         return True
-
-#     def display_balance(self):
-#         print(f"Current Balance: ${self.__account_balance:.2f}")
-        
-        
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.__account_balance = initial_balance  # Private attribute to encapsulate balance
+        self.__account_balance = initial_balance 
 
     def deposit(self, amount):
         if amount > 0:
