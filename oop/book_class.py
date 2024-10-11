bf9fe5b76afccff8f776215c6b01d82623e49910
alikurules <alikurules@gmail.com>
2024-10-11 17:13:55 +0300

    Dive into Python Magic Methods

--- oop/book_class.py
+++ oop/book_class.py
@@ -2,7 +2,7 @@ class Book:
     def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
-        self.publication_year = year
+        self.year = year
         
     def __del__(self):
         print(f"Deleting {self.title}")
